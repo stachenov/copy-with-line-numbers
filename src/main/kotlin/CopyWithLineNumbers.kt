d37de180fec131f71a5989e058635a357f9384bd
Sergei Tachenov <sergei@tachenov.name>
2020-03-07 13:16:44 +0300

    Code cleanup before trying to implement anything

--- src/main/kotlin/CopyWithLineNumbers.kt
+++ src/main/kotlin/CopyWithLineNumbers.kt
@@ -1,53 +1,49 @@
 package name.tachenov.intellij.plugins.copyWithLineNumbers
 
 import com.intellij.codeInsight.editorActions.CopyPastePostProcessor
-import com.intellij.codeInsight.editorActions.CopyPastePreProcessor
 import com.intellij.codeInsight.editorActions.TextBlockTransferable
 import com.intellij.codeInsight.editorActions.TextBlockTransferableData
-import com.intellij.ide.DataManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.editor.RawText
 import com.intellij.openapi.editor.impl.EditorCopyPasteHelperImpl
+import com.intellij.openapi.editor.richcopy.view.HtmlTransferableData
+import com.intellij.openapi.editor.richcopy.view.RtfTransferableData
 import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.DumbService
 import com.intellij.psi.PsiDocumentManager
-import java.awt.datatransfer.Transferable
 import java.util.*
 
-class CopyWithLineNumbers : AnAction() {
+class CopyWithLineNumbers : AnAction(), DumbAware {
     override fun actionPerformed(e: AnActionEvent) {
-        // Mostly shamelessly copied from com.intellij.codeInsight.editorActions.CopyHandler.doExecute()
+        // I really dislike copy-paste programming, but my knowledge of IntelliJ Platform is very limited,
+        // so this was mostly shamelessly copied from com.intellij.codeInsight.editorActions.CopyHandler.doExecute()
         val editor = CommonDataKeys.EDITOR.getData(e.dataContext) ?: return
-        val project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(editor.component)) ?: return
+        val project = CommonDataKeys.PROJECT.getData(e.dataContext) ?: return
         val file = PsiDocumentManager.getInstance(project).getPsiFile(editor.document)
         val selectionModel = editor.selectionModel
         PsiDocumentManager.getInstance(project).commitAllDocuments()
         val startOffsets = selectionModel.blockSelectionStarts
         val endOffsets = selectionModel.blockSelectionEnds
-        val transferableDataList: MutableList<TextBlockTransferableData> = ArrayList()
+        val transferableDataList = ArrayList<TextBlockTransferableData>()
         DumbService.getInstance(project).withAlternativeResolveEnabled {
             for (processor in CopyPastePostProcessor.EP_NAME.extensionList) {
                 transferableDataList.addAll(processor.collectTransferableData(file, editor, startOffsets, endOffsets))
             }
         }
-        val text = (if (editor.caretModel.supportsMultipleCarets())
-            EditorCopyPasteHelperImpl.getSelectedTextForClipboard(editor, transferableDataList)
-        else
-            selectionModel.selectedText)
-        ?: return
-        val rawText = TextBlockTransferable.convertLineSeparators(text, "\n", transferableDataList)!!
-        var escapedText: String? = null
-        for (processor in CopyPastePreProcessor.EP_NAME.extensionList) {
-            escapedText = processor.preprocessOnCopy(file, startOffsets, endOffsets, rawText)
-            if (escapedText != null)
-                break
-        }
-        val transferable = TextBlockTransferable(escapedText ?: rawText,
-                transferableDataList,
-                if (escapedText != null) RawText(rawText) else null)
+        transferableDataList.retainAll { it is RtfTransferableData || it is HtmlTransferableData }
+        var text = if (editor.caretModel.supportsMultipleCarets())
+                EditorCopyPasteHelperImpl.getSelectedTextForClipboard(editor, transferableDataList)
+            else
+                selectionModel.selectedText
+        text = TextBlockTransferable.convertLineSeparators(text, "\n", transferableDataList)!!
+        text = addLineNumbers(text, transferableDataList)
+        val transferable = TextBlockTransferable(text, transferableDataList, null)
         CopyPasteManager.getInstance().setContents(transferable)
+    }
 
+    private fun addLineNumbers(text: String, transferableDataList: ArrayList<TextBlockTransferableData>): String {
+        return text
     }
 }
